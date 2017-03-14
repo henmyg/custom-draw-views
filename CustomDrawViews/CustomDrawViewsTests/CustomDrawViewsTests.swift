1580d8d2f72add2b460975028dbffe1b8d4c9bbc
Henrik Top Mygind <github@henmyg.dk>
2017-03-14 20:23:01 +0100

    Added empty project

---
+++ CustomDrawViews/CustomDrawViewsTests/CustomDrawViewsTests.swift
@@ -0,0 +1,14 @@
+//
+//  CustomDrawViewsTests.swift
+//  CustomDrawViewsTests
+//
+//  Created by Henrik Top Mygind on 14/03/2017.
+//  Copyright © 2017 Henrik Top Mygind. All rights reserved.
+//
+
+import XCTest
+@testable import CustomDrawViews
+
+class CustomDrawViewsTests: XCTestCase {
+    
+}
